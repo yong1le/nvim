7b11f027db71a66c91de1cd89b2cac10a77c9d7f
yong1le <ygle.he@gmail.com>
2023-02-15 02:40:23 -0500

    update colorscheme to one dark pro

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.guifont = "FiraCode Nerd Font:h15"
-vim.g.neovide_background_color = "#313446"
+vim.opt.guifont = "OperatorMono Nerd Font:h16"
+vim.g.neovide_background_color = "#30343c"
 vim.g.neovide_floating_blur_amount_x = 2.0
 vim.g.neovide_floating_blur_amount_y = 2.0
 vim.g.neovide_remember_window_size = true
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,59 +1,18 @@
 return {
   {
-    {
-      "catppuccin/nvim",
-      name = "catppuccin",
-      config = function()
-        require("catppuccin").setup({
-          flavour = "frappe", -- latte, frappe, macchiato, mocha
-          background = {
-            light = "latte",
-            dark = "mocha",
-          },
-          transparent_background = false,
-          show_end_of_buffer = false,
-          term_colors = false,
-          dim_inactive = {
-            enabled = false,
-            shade = "dark",
-            percentage = 0.15,
-          },
-          no_italic = false,
-          no_bold = false,
-          styles = {
-            comments = { "italic" },
-            conditionals = { "italic" },
-            loops = {},
-            functions = {},
-            keywords = { "bold" },
-            strings = {},
-            variables = {},
-            numbers = { "bold" },
-            booleans = { "bold" },
-            properties = {},
-            types = {},
-            operators = {},
-          },
-          color_overrides = {},
-          custom_highlights = {},
-          integrations = {
-            cmp = true,
-            gitsigns = true,
-            nvimtree = true,
-            telescope = true,
-            notify = false,
-            mini = false,
-            -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-          },
-        })
-        vim.cmd.colorscheme("catppuccin")
-      end,
-    },
-    {
-      "LazyVim/LazyVim",
-      opts = {
-        colorscheme = "catppuccin",
-      },
+    "olimorris/onedarkpro.nvim",
+    config = function ()
+      require('onedarkpro').setup({
+        options = {
+          cursorline = true,
+        }
+      })
+    end
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark_vivid",
     },
   },
 }
