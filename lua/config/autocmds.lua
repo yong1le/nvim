e6bbd3cb954b3bf9f6c8b241ea039e34dc1e975a
yong1le <ygle.he@gmail.com>
2023-02-26 14:56:17 -0500

    jump out of snippet tab complete in normal mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -31,3 +31,15 @@ vim.api.nvim_create_autocmd('BufFilePost', {
     vim.api.nvim_buf_set_option(0, "commentstring", "-- %s")
   end
 })
+
+vim.api.nvim_create_autocmd('ModeChanged', {
+  pattern = '*',
+  callback = function()
+    if ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+        and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require('luasnip').session.jump_active
+    then
+      require('luasnip').unlink_current()
+    end
+  end
+})
