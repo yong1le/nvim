ceea0a2a30552751ed6dcefd41f12c9d919c9f84
yong1le <ygle.he@gmail.com>
2023-03-07 00:37:23 -0500

    remove debugging plugins

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,34 +1,34 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-    opts = {
-      ensure_installed = {},
-    },
-    config = function(plugin, opts)
-      require("mason").setup(opts)
-      local mr = require("mason-registry")
-      for _, tool in ipairs(opts.ensure_installed) do
-        local p = mr.get_package(tool)
-        if not p:is_installed() then
-          p:install()
-        end
-      end
-    end,
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-      "williamboman/mason.nvim",
-    },
-    config = function()
-      require('mason').setup({})
-      require("mason-nvim-dap").setup({
-        automatic_setup = true,
-      })
-      require 'mason-nvim-dap'.setup_handlers {}
-    end,
-  },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   cmd = "Mason",
+  --   keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+  --   opts = {
+  --     ensure_installed = {},
+  --   },
+  --   config = function(plugin, opts)
+  --     require("mason").setup(opts)
+  --     local mr = require("mason-registry")
+  --     for _, tool in ipairs(opts.ensure_installed) do
+  --       local p = mr.get_package(tool)
+  --       if not p:is_installed() then
+  --         p:install()
+  --       end
+  --     end
+  --   end,
+  -- },
+  -- {
+  --   "jay-babu/mason-nvim-dap.nvim",
+  --   dependencies = {
+  --     "mfussenegger/nvim-dap",
+  --     "williamboman/mason.nvim",
+  --   },
+  --   config = function()
+  --     require('mason').setup({})
+  --     require("mason-nvim-dap").setup({
+  --       automatic_setup = true,
+  --     })
+  --     require 'mason-nvim-dap'.setup_handlers {}
+  --   end,
+  -- },
 }
