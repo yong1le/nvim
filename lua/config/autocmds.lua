00992cafdc3e427be923ec38fc1904581a7cabb5
yong1le <ygle.he@gmail.com>
2023-06-24 17:26:33 -0400

    disable on filetype

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -10,4 +10,21 @@
 --     end
 --   end,
 -- })
---
+
+-- disable indent guides on certain buffer types
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "neo-tree", "toggleterm", "alpha", "Trouble", "dashboard", "lazy", "mason", "help"},
+  callback = function ()
+    vim.b.miniindentscope_disable = true
+  end
+
+})
+
+-- disable statusline on certain buffer types
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "neo-tree", "toggleterm", "alpha", "Trouble", "dashboard", "lazy", "mason"},
+  callback = function ()
+    vim.b.ministatusline_disable = true
+  end
+
+})
