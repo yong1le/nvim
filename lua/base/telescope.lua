faec6e36e84133ae75c4924d9b55bda7fa9fb7e8
yong1le <ygle.he@gmail.com>
2023-06-24 17:06:22 -0400

    change telescope layout

--- lua/base/telescope.lua
+++ lua/base/telescope.lua
@@ -11,5 +11,24 @@ return {
     { "<leader>fm",  "<cmd>lua require('telescope.builtin').man_pages{}<cr>",    desc = "Man Pages" },
     { "<leader>fgf", "<cmd>lua require('telescope.builtin').git_files{}<cr>",    desc = "Git Files" },
     { "<leader>fgb", "<cmd>lua require('telescope.builtin').git_branches{}<cr>", desc = "Git Branches" },
-  }
+  },
+  config = function()
+    require 'telescope'.setup({
+      defaults = {
+        sorting_strategy = "ascending",
+        layout_strategy = "center",
+        layout_config = {
+          preview_cutoff = 1, -- Preview should always show (unless previewer = false)
+
+          width = function(_, max_columns, _)
+            return math.min(max_columns, 80)
+          end,
+
+          height = function(_, _, max_lines)
+            return math.min(max_lines, 15)
+          end,
+        },
+      }
+    })
+  end
 }
