fc91b055a49df2c56f21fd222427cbe660c99700
yong1le <ygle.he@gmail.com>
2023-06-30 23:56:41 -0400

    change notes config

--- lua/base/notes.lua
+++ lua/base/notes.lua
@@ -1,30 +1,47 @@
 return {
-  "mickael-menu/zk-nvim",
-  cmd = {
-    "ZkNew", "ZkIndex", "ZkNotes", "ZkTags",
-  },
-  keys = {
-    { "<leader>zo", "<cmd>ZkNotes<cr>",                                   desc = "Open Notes" },
-    { "<leader>zi", "<cmd>ZkInsertLink<cr>",                              desc = "Insert Link" },
-    { "<leader>zl", "<cmd>ZkLinks<cr>",                                   desc = "Links" },
-    { "<leader>zb", "<cmd>ZkBacklinkds<cr>",                              desc = "Backlinks" },
-    { "<leader>zt", "<cmd>ZkTags<cr>",                                    desc = "Open Tags" },
-    { "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", desc = "New Note" },
+  {
+    "mickael-menu/zk-nvim",
+    cmd = {
+      "ZkNew", "ZkIndex", "ZkNotes", "ZkTags",
+    },
+    keys = {
+      { "<leader>zo", "<cmd>ZkNotes<cr>",                                   desc = "Open Notes" },
+      { "<leader>zi", "<cmd>ZkInsertLink<cr>",                              desc = "Insert Link" },
+      { "<leader>zl", "<cmd>ZkLinks<cr>",                                   desc = "Links" },
+      { "<leader>zb", "<cmd>ZkBacklinkds<cr>",                              desc = "Backlinks" },
+      { "<leader>zt", "<cmd>ZkTags<cr>",                                    desc = "Open Tags" },
+      { "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", desc = "New Note" },
 
+    },
+    config = function()
+      require("zk").setup({
+        picker = "telescope",
+        lsp = {
+          config = {
+            cmd = { "zk", "lsp" },
+            name = "zk",
+          },
+          auto_attach = {
+            enabled = true,
+            filetypes = { "markdown" },
+          },
+        },
+      })
+    end
   },
-  config = function()
-    require("zk").setup({
-      picker = "telescope",
-      lsp = {
-        config = {
-          cmd = { "zk", "lsp" },
-          name = "zk",
+  {
+    "postfen/clipboard-image.nvim",
+    keys = {
+      { "<leader>zp", "<cmd>PasteImg<cr>", desc = "Paste Image" },
+    },
+    config = function()
+      require 'clipboard-image'.setup {
+        default = {
+          img_dir = "images",
+          img_name = function() return os.date('%Y-%m-%d-%H-%M-%S') end,
+          affix = "<\n  %s\n>"
         },
-        auto_attach = {
-          enabled = true,
-          filetypes = { "markdown" },
-        },
-      },
-    })
-  end
+      }
+    end
+  },
 }
