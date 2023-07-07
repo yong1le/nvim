61a2e959dcd08084dabe0b634cff08002adb6d5a
yong1le <ygle.he@gmail.com>
2023-07-07 11:10:11 -0400

    correct keybinding

--- lua/base/coding.lua
+++ lua/base/coding.lua
@@ -56,7 +56,7 @@ return {
     keys = {
       { '<leader>th', '<cmd>ToggleTerm direction=horizontal<cr>',        desc = 'Horizontal Terminal' },
       { '<leader>tv', '<cmd>ToggleTerm direction=vertical<cr>',          desc = 'Vertical Terminal' },
-      { '<leader>tl', '<cmd>TermExec cmd="lazygit" direction=float<cr>', desc = 'Lazygit' },
+      { '<leader>tf', '<cmd>ToggleTerm direction=float<cr>',             desc = 'Floating Terminal' },
     },
     config = function()
       require('toggleterm').setup({
