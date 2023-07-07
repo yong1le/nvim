8685ddba2e5a3f0f46b26485eff5aca8e9240575
yong1le <ygle.he@gmail.com>
2023-07-07 01:42:32 -0400

    put config in sep file

--- lua/base/editor.lua
+++ lua/base/editor.lua
@@ -12,63 +12,7 @@ return {
       'MunifTanjim/nui.nvim',
     },
     config = function()
-      require('neo-tree').setup({
-        close_if_last_window = true,
-        popup_border_style = 'rounded',
-        enable_git_status = true,
-        open_files_do_not_replace_types = { "terminal", "trouble", "qf", "toggleterm" },
-        default_component_configs = {
-          git_status = {
-            symbols = {
-              renamed = "󰁕",
-              unstaged = "󰄱",
-            }
-          },
-          icon = {
-            folder_empty = "󰜌",
-            folder_empty_open = "󰜌",
-          },
-          indent = {
-            with_expanders = true,
-            expander_collapsed = "",
-            expander_expanded = "",
-            expander_highlight = "NeoTreeExpander",
-          },
-        },
-        window = {
-          position = 'left',
-          width = 30,
-          mappings = {
-            ["<space>"] = { "toggle_node", nowait = false },
-            ["<2-LeftMouse>"] = "open",
-            ["<cr>"] = "open",
-            ["<esc>"] = "revert_preview",
-            ["P"] = { "toggle_preview", config = { use_float = true } },
-            ["l"] = "focus_preview",
-            ["S"] = "open_split",
-            ["v"] = "open_vsplit",
-            ["t"] = "open_tabnew",
-            ["w"] = "open_with_window_picker",
-            ["C"] = "close_node",
-            ["z"] = "close_all_nodes",
-            ["a"] = { "add", config = { show_path = "none" } },
-            ["A"] = "add_directory",
-            ["d"] = "delete",
-            ["r"] = "rename",
-            ["y"] = "copy_to_clipboard",
-            ["x"] = "cut_to_clipboard",
-            ["p"] = "paste_from_clipboard",
-            ["c"] = "copy",
-            ["m"] = "move",
-            ["q"] = "close_window",
-            ["R"] = "refresh",
-            ["?"] = "show_help",
-            ["<"] = "prev_source",
-            [">"] = "next_source",
-          }
-        }
-
-      })
+      require('base.config.neotree')
     end
   },
   {
--- lua/base/ui.lua
+++ lua/base/ui.lua
@@ -2,53 +2,8 @@ return {
   {
     "goolord/alpha-nvim",
     event = "VimEnter",
-    opts = function()
-      local dashboard = require("alpha.themes.dashboard")
-      local logo = [[Hello yong1le]]
-
-      dashboard.section.header.val = vim.split(logo, "\n")
-      dashboard.section.buttons.val = {
-        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-        dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-        dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-        dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-        -- dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
-        dashboard.button("q", " " .. " Quit", ":qa<CR>"),
-      }
-      for _, button in ipairs(dashboard.section.buttons.val) do
-        button.opts.hl = "AlphaButtons"
-        button.opts.hl_shortcut = "AlphaShortcut"
-      end
-      dashboard.section.header.opts.hl = "AlphaHeader"
-      dashboard.section.buttons.opts.hl = "AlphaButtons"
-      dashboard.section.footer.opts.hl = "AlphaFooter"
-      dashboard.opts.layout[1].val = 8
-      return dashboard
-    end,
-    config = function(_, dashboard)
-      -- close Lazy and re-open when the dashboard is ready
-      if vim.o.filetype == "lazy" then
-        vim.cmd.close()
-        vim.api.nvim_create_autocmd("User", {
-          pattern = "AlphaReady",
-          callback = function()
-            require("lazy").show()
-          end,
-        })
-      end
-
-      require("alpha").setup(dashboard.opts)
-
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "LazyVimStarted",
-        callback = function()
-          local stats = require("lazy").stats()
-          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          dashboard.section.footer.val = "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
-          pcall(vim.cmd.AlphaRedraw)
-        end,
-      })
+    config = function()
+      require('base.config.dashboard')
     end,
   },
   {
@@ -87,48 +42,7 @@ return {
     priority = 1000,
     lazy = false,
     config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-        background = {
-          light = "latte",
-          dark = "mocha",
-        },
-        transparent_background = true,
-        show_end_of_buffer = false,
-        term_colors = false,
-        dim_inactive = {
-          enabled = false,
-          shade = "dark",
-          percentage = 0.15,
-        },
-        no_italic = false,
-        no_bold = false,
-        no_underline = false,
-        styles = {
-          comments = { "italic" },
-          conditionals = { "italic" },
-          loops = { "bold" },
-          functions = { "bold" },
-          keywords = { "italic" },
-          strings = {},
-          variables = {},
-          numbers = {},
-          booleans = {},
-          properties = {},
-          types = {},
-          operators = {},
-        },
-        color_overrides = {},
-        custom_highlights = {},
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          telescope = true,
-          notify = false,
-          mini = false,
-        },
-      })
+      require('base.config.catppuccin')
     end
   },
   {
