bcbabd1e947d456b3eba4dee58e35d57cce29ca6
yong1le <ygle.he@gmail.com>
2023-08-24 19:20:58 -0400

    add icons to statusline

--- lua/base/ui/statusbar.lua
+++ lua/base/ui/statusbar.lua
@@ -6,6 +6,45 @@ return {
   },
   event = "BufEnter",
   opts = function()
+    -- Getting attached lsp clients
+    local get_clients = function()
+      local clients = vim.lsp.buf_get_clients()
+      local lsp_string = ""
+      for i, client in ipairs(clients) do
+        -- Don't show full if too much clients
+        if i >= 3 then
+          lsp_string = lsp_string .. ""
+          break
+        end
+        if i == 1 then
+          lsp_string = client.name
+        else
+          lsp_string = lsp_string .. ", " .. client.name
+        end
+      end
+      return lsp_string
+    end
+
+    local mode_icons = {
+      NORMAL = "󰽻",
+      ["O-PENDING"] = "󰥔",
+      VISUAL = "󰆐",
+      ["V-LINE"] = "󰆐",
+      ["V-BLOCK"] = "󰆐",
+      SELECT = "󰌇",
+      ["S-LINE"] = "󰌇",
+      ["S-BLOCK"] = "󰌇",
+      INSERT = "󰏫",
+      REPLACE = "󰛔",
+      ["V-REPLACE"] = "󰛔",
+      COMMAND = "󰘳",
+      EX = "󰘳",
+      MORE = "",
+      CONFIRM = "",
+      SHELL = "",
+      TERMINAL = ""
+    }
+
     return {
       options = {
         icons_enabled = true,
@@ -24,7 +63,7 @@ return {
           {
             'mode',
             fmt = function(str)
-              return str:sub(1, 1)
+              return mode_icons[str]
             end
           }
         },
@@ -33,7 +72,7 @@ return {
           'branch',
           {
             'diff',
-            symbols = { added = " ", modified = " ", remove = " "},
+            symbols = { added = " ", modified = " ", remove = " " },
           },
           'filetype' },
         lualine_x = {
@@ -41,7 +80,12 @@ return {
             "diagnostics",
             sources = { "nvim_lsp", 'nvim_diagnostic' },
           },
-          "location" },
+          {
+            get_clients,
+            icon = ""
+          },
+          "location"
+        },
         lualine_y = {},
         lualine_z = { 'progress' }
       },
