b1d7d3bfb66457939a5e37b607cd6c5db50e1197
yong1le <ygle.he@gmail.com>
2023-08-23 23:40:14 -0400

    change status col

--- lua/base/ui/statuscol.lua
+++ lua/base/ui/statuscol.lua
@@ -2,11 +2,18 @@ return {
   {
     "luukvbaal/statuscol.nvim",
     opts = {
+      ft_ignore = { "neo-tree", "toggleterm", "alpha", "Trouble", "dashboard", "lazy", "mason", "help"},
+      bt_ignore = { "neo-tree", "toggleterm", "alpha", "Trouble", "dashboard", "lazy", "mason", "help"},
+
       relculright = true,
       segments = {
+        -- numbercol
+        {text = {require("statuscol.builtin").lnumfunc, " "}, click = "v:lua.ScLa"},
+        -- Gitsigns
+        {text = {"%s"}, click = "v:lua.ScSa"},
+        -- Foldcol
         {text = {require("statuscol.builtin").foldfunc}, click = "v:lua.ScFa"},
-        {text = {" %s "}, click = "v:lua.ScSa"},
-        {text = {require("statuscol.builtin").lnumfunc, " "}, click = "v:lua.ScLa"}
+        {text = {"│"}},
       }
     },
     config = function(_, opts)
