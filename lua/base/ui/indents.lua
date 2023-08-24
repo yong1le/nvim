de0d71c02bd7899ebf82555ef958a1ad4a1457fd
yong1le <ygle.he@gmail.com>
2023-08-24 17:51:22 -0400

    format

--- lua/base/ui/indents.lua
+++ lua/base/ui/indents.lua
@@ -1,17 +1,26 @@
 return {
-    {
+  {
     'echasnovski/mini.indentscope',
     version = '*',
     lazy = false,
     opts = {
-        options = {
-          indent_at_cursor = false,
-          try_as_border = true,
-        },
-        symbol = "│",
+      options = {
+        indent_at_cursor = false,
+        try_as_border = true,
+      },
+      symbol = "│",
     },
     config = function(_, opts)
       require('mini.indentscope').setup(opts)
+
+      -- disable indent guides on certain buffer types
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "neo-tree", "toggleterm", "alpha", "Trouble", "dashboard", "lazy", "mason", "help" },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end
+
+      })
     end
   },
 }
