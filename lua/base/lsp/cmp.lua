fd1f4c9af0f02bb322992fd77ccd459c165d2de3
yong1le <ygle.he@gmail.com>
2023-08-24 17:51:06 -0400

    polish tab

--- lua/base/lsp/cmp.lua
+++ lua/base/lsp/cmp.lua
@@ -27,10 +27,34 @@ return {
         },
         mapping = cmp.mapping.preset.insert({
           ["<CR>"] = cmp.mapping.confirm({ select = false }),
-          ["<Tab>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehaviorInsert },
-          ["<S-Tab>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehaviorInsert },
-          ["<C-n>"] = cmp.mapping(function() snip.expand_or_jump() end, { 'i', 's' }),
-          ["<C-p>"] = cmp.mapping(function() snip.jump(-1) end, { 'i', 's' })
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif snip.expand_or_jumpable() then
+              snip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif snip.jumpable(-1) then
+              snip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ["<C-n>"] = cmp.mapping(function()
+            if snip.expand_or_jumpable() then
+              snip.expand_or_jump()
+            end
+          end, { 'i', 's' }),
+          ["<C-p>"] = cmp.mapping(function()
+            if snip.jumpable(-1) then
+              snip.jump(-1)
+            end
+          end, { 'i', 's' })
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp", priority = 1000 },
@@ -61,7 +85,8 @@ return {
     },
     event = { "InsertEnter" },
     opts = {
-      history = true
+      history = true,
+      region_check_events = { "InsertEnter" },
     },
     config = function(_, opts)
       require("luasnip").config.setup(opts)
