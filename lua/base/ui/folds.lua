db4658b11f8bb0a87b0ee9f5464aee066eed2f25
yong1le <ygle.he@gmail.com>
2023-08-28 12:08:27 -0400

    update

--- lua/base/ui/folds.lua
+++ lua/base/ui/folds.lua
@@ -1,18 +1,22 @@
 return {
   {
     "kevinhwang91/nvim-ufo",
-    event = { "BufReadPost", "BufNewFile"},
+    event = { "BufReadPost", "BufNewFile" },
     dependencies = {
-      'kevinhwang91/promise-async',
+      "kevinhwang91/promise-async",
       "luukvbaal/statuscol.nvim",
+    },
+    keys = {
+      { "<zR>", "<cmd>lua require('ufo').openAllFolds()<cr>", desc = "Open All Folds" },
+      { "<zM>", "<cmd>lua require('ufo').closeAllFolds()<cr>", desc = "Close All Folds" },
     },
     opts = {
       provider_selector = function(bufnr, filetype, buftype)
-        return {'treesitter', 'indent'}
+        return { "treesitter", "indent" }
       end,
     },
     config = function(_, opts)
       require("ufo").setup(opts)
-    end
-  }
+    end,
+  },
 }
