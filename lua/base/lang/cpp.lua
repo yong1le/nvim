f530c54d1f8e3c0847f3cf991638ce5310d25c69
yong1le <ygle.he@gmail.com>
2023-10-01 14:01:14 -0400

    remove formatter for cpp

--- lua/base/lang/cpp.lua
+++ lua/base/lang/cpp.lua
@@ -13,12 +13,12 @@ return {
       opts.ensure_installed = utils.insert_unique(opts.ensure_installed, { "c", "cpp" })
     end,
   },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = utils.insert_unique(opts.ensure_installed, { "clang-format" })
-    end,
-  },
+  -- {
+  --   "WhoIsSethDaniel/mason-tool-installer.nvim",
+  --   opts = function(_, opts)
+  --     opts.ensure_installed = utils.insert_unique(opts.ensure_installed, {})
+  --   end,
+  -- },
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
@@ -29,16 +29,16 @@ return {
       })
     end,
   },
-  {
-    "creativenull/efmls-configs-nvim",
-    opts = function(_, opts)
-      local clang_format = require "efmls-configs.formatters.clang_format"
-
-      local languages = { "c", "cpp" }
-      opts.filetypes = utils.insert_unique(opts.filetypes, languages)
-      for _, l in ipairs(languages) do
-        opts.settings.languages[l] = utils.insert_unique(opts.settings.languages[l], { clang_format })
-      end
-    end,
-  },
+  -- {
+  --   "creativenull/efmls-configs-nvim",
+  --   opts = function(_, opts)
+  --     local clang_format = require "efmls-configs.formatters.clang_format"
+  --
+  --     local languages = { "c", "cpp" }
+  --     opts.filetypes = utils.insert_unique(opts.filetypes, languages)
+  --     for _, l in ipairs(languages) do
+  --       opts.settings.languages[l] = utils.insert_unique(opts.settings.languages[l], { clang_format })
+  --     end
+  --   end,
+  -- },
 }
