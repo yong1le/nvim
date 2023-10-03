10e66182b5f1737d6c88a8fac98588453a29656a
yong1le <ygle.he@gmail.com>
2023-10-03 17:44:25 -0400

    add files to not illuminate

--- lua/base/ui/highlight.lua
+++ lua/base/ui/highlight.lua
@@ -2,7 +2,14 @@ return {
   {
     "RRethy/vim-illuminate",
     event = { "BufReadPost", "BufNewFile" },
-    opts = {},
+    opts = {
+      filetypes_denylist = {
+        'norg',
+        'tex',
+        'latex',
+        'markdown',
+      }
+    },
     config = function (_, opts)
       require("illuminate").configure(opts)
     end
