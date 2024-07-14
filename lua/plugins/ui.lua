f886fe2b86160bda251e1541dd26ca63e4ff3522
Yong Le He <ygle.he@gmail.com>
2024-07-14 13:33:27 -0400

    kanagawa

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,17 +1,41 @@
 return {
   {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000,
+    "rebelot/kanagawa.nvim",
     lazy = false,
+    priority = 9999,
     opts = {
-      options = {
-        transparency = true
-      }
+      compile = false,
+      undercurl = true,
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false,
+      dimInactive = false,
+      terminalColors = true,
+      colors = {
+        palette = {},
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none",
+            },
+          },
+        },
+      },
+      overrides = function(colors)
+        return {
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+        }
+      end,
     },
     config = function(_, opts)
-      require("onedarkpro").setup(opts)
-      vim.cmd([[colorscheme onedark]])
-    end
+      require("kanagawa").setup(opts)
+      require("kanagawa").load("wave")
+    end,
   },
 
   -- Better select prompts
